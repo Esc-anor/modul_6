1c8935f740e68176187451b7d8fce5a4eefd9fbf
Роман <romangribanov@rambler.ru>
2024-10-10 17:19:16 +0300

    Исправлено

--- module_6_1.py
+++ module_6_1.py
@@ -1,24 +1,13 @@
 # Создайте 2 класса родителя: Animal, Plant
 class Animal:
+    # Для класса Animal атрибуты alive = True (живой) и fed = False (не накормленный)
+    alive = True
+    fed = False
+
     def __init__(self, name):
-        # Для класса Animal атрибуты alive = True (живой) и fed = False (не накормленный),
-        # name - индивидуальное название каждого животного.
-        self.alive = True
-        self.fed = False
-        self.name = name
+        self.name = name  # name - индивидуальное название каждого животного.
 
-
-class Plant:
-    def __init__(self, name, edible=False):
-        # Для класса Plant атрибут edible = False (съедобность), name - индивидуальное название каждого растения
-        self.edible = edible  # По умолчанию растение несъедобное
-        self.name = name
-
-
-# Класс - наследник Mammal (Млекопитающие) для класса Животных.
-class Mammal(Animal):
-    # Метод eat для млекопитающих, позволяющий им есть растения
-    def eat(self, food):
+    def eat(self, food):  # Метод eat позволяющий им есть растения
         if isinstance(food, Plant):  # Проверяем, является ли еда растением
             if food.edible:  # Если растение съедобное
                 print(f"{self.name} съел {food.name}")  # Животное съело растение
@@ -26,38 +15,37 @@ class Mammal(Animal):
             else:
                 print(f"{self.name} не стал есть {food.name}")
                 self.alive = False  # Животное погибло из-за несъедобного растения
-        else:
-            # Если еда не является растением
-            print(f"{food.name} не является растением. {self.name} не может это съесть.")
-
-
-# Класс - наследник Predatar (Хищники) для класса Животных.
-class Predator(Animal):
-    # Метод eat для хищников, позволяющий им есть растения
-    def eat(self, food):
-        if isinstance(food, Plant):  # Проверяем, является ли еда растением
-            if food.edible:  # Если растение съедобное
-                print(f"{self.name} съел {food.name}")  # Хищник съел растение
-                self.fed = True  # Хищник накормлен
-            else:
-                print(f"{self.name} не стал есть {food.name}")  # Хищник отказался от еды
-                self.alive = False  # Хищник погиб из-за несъедобного растения
         else:  # Если еда не является растением
             print(f"{food.name} не является растением. {self.name} не может это съесть.")
 
 
-# Класс - наследник Flower (Цветы) для класса Растений.
-class Flower(Plant):
-    def __init__(self, name):
-        # Вызываем конструктор родительского класса Plant и устанавливаем, что цветы несъедобные
-        super().__init__(name, edible=False)
+class Predator(Animal):  # Класс - наследник Predatar (Хищники) для класса Животных.
+    pass
 
 
-# Класс - наследник Fruit (Фрукты) для класса Растений.
-class Fruit(Plant):
+class Mammal(Animal):  # Класс - наследник Mammal (Млекопитающие) для класса Животных.
+    pass
+
+
+class Plant:
+    edible = False  # Для класса Plant атрибут edible = False (съедобность)
+
     def __init__(self, name):
-        # Вызываем конструктор родительского класса Plant и устанавливаем, что фрукты съедобные
-        super().__init__(name, edible=True)
+        self.name = name  # name - индивидуальное название каждого растения
+
+
+class Flower(Plant):  # Класс - наследник Flower (Цветы) для класса Растений.
+    edible = False  # Вызываем конструктор родительского класса Plant и устанавливаем, что цветы несъедобные
+
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Fruit(Plant):  # Класс - наследник Fruit (Фрукты) для класса Растений.
+    edible = True  # Вызываем конструктор родительского класса Plant и устанавливаем, что фрукты съедобные
+
+    def __init__(self, name):
+        super().__init__(name)
 
 
 a1 = Predator('Волк с Уолл-Стрит')
